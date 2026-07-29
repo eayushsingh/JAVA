2b6788a147a6495afe8c17716f45b6d6b9012448
Ayush Singh <ayushhsinghhe01@gmail.com>
2026-07-29 16:25:54 +0530

    Implement counting of digit '7' in CountNum.java
    Added functionality to count occurrences of the digit '7' in a number.

--- CountNum.java
+++ CountNum.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+class CountNumbers{
+public static void main(String[] args){
+   Scanner sc = new Scanner(System.in);
+   int count=0;
+   int n= sc.nextInt();
+   while(n>0){
+       int rem = n%10;
+       if(rem==7){
+           count++;
+       }
+       n=n/10;
+   }
+   System.out.println(count);
+}
+}
