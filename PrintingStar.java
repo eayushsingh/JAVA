fac7e21a1403d2cf260c2ebf685b8bc23b171193
Ayush Singh <ayushhsinghhe01@gmail.com>
2026-08-05 19:00:21 +0530

    Add star printing program in PrintingStar.java
    Implement a program to print stars in a triangular pattern based on user input.

---
+++ PrintingStar.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            for(int row = 1;row<=n;row++){
+                for(int col = 1; col<=row;col++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
