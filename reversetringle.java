9f4a38c5a0e0db1accdb3d236e5d44882b26b621
Ayush Singh <ayushhsinghhe01@gmail.com>
2026-08-06 21:00:25 +0530

    Implement reverse triangle pattern in reversetringle.java
    Added a Java program to print a reverse triangle pattern based on user input.

--- reversetringle.java
+++ reversetringle.java
@@ -1,1 +1,25 @@
+/*   * * * * * 
+    * * * 
+    * * 
+    *            
+                 */
 
+
+
+
+import java.util.Scanner;
+class Ayush{
+
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+for(int row =1  ; row <=n; row++){
+  for(int col =1 ; col <= n - row +1; col++){
+    System.out.print("* ");
+    
+}
+  System.out.println();
+  
+}
+}
+}
